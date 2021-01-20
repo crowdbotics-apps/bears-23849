e94817e27b734b6691a3d35e973807fbe0da1de3
cappsjulian <cappsjulian+bob@gmail.com>
2021-01-20 11:33:38 -0500

    Update screen

--- src/features/BlankScreen0194789/screens/blank.js
+++ src/features/BlankScreen0194789/screens/blank.js
@@ -1,41 +1,31 @@
-import React from "react";
-import {
-  View,
-  Image,
-  ImageBackground,
-  TouchableOpacity,
-  Text,
-  Button,
-  Switch,
-  TextInput,
-  StyleSheet,
-  ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
-
-export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
-    return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
+import React, { Component } from 'react'
+import { AppRegistry, View, Button, StyleSheet } from 'react-native'
+class App extends Component {
+  state = {
+      count: 0,
+  };
+  handleCounter = () => {
+     this.setState({"count": this.state.count + 1})
   };
   
-  state = {};
-
-  render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
-    </View>
-  );
+  render() {
+      const { count } = this.state;
+      return (
+          <View style={styles.container}>
+                 <Button onPress={this.handleCounter}>Increment</Button>
+             <Button onPress={this.handleCounter}>Decrement</Button>
+            <Button onPress={this.handleCounter}>Reset</Button>
+            <Button onPress={this.handleCounter}>Increment by 10</Button>
+          </View>
+      );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    justifyContent: "center",
+    alignItems: "stretch",
+    padding: 20
   },
-});
+})
+AppRegistry.registerComponent('App', () => App)
